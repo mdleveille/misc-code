bfb031f13acd84ffe00929386e69cf95f52b2c24
Michael Leveille <mleveille@stsci.edu>
2018-05-08 13:57:55 -0400

    change calculation to be inclusive of payday

--- vacation_time.py
+++ vacation_time.py
@@ -1,4 +1,15 @@
 from datetime import date, timedelta
+"""
+This script will calculate the amount of vacation time you will have accrued by a give date.
+
+Inputs:
+    - Desired vacation date
+    - Current amount of vacation time you have
+
+** Notes **
+    - If your desired vacation date happens to be a pay day, the vacation time will include the time you recieve on that pay day.
+        - EX: If you have zero vacation time at the start of a pay period and you want to take time off the Friday after next (pay day) you will have accrued 7.3 hours by that Friday.
+"""
 
 # Prompt user for desired vaction day and create targetDate datetime object
 while True:
@@ -10,7 +21,7 @@ while True:
         targetDay = date(int(vacationDate[6:]), int(vacationDate[:2]), int(vacationDate[3:5]))
 
     except ValueError:
-        print("Please check your format and try again.\n")
+        print("Please check the format and try again.\n")
         continue
 
     else:
@@ -24,7 +35,7 @@ now = date.today()
 diff = targetDay - now
 
 # Make list of datetime objects for all days from now until targetDay
-days = [now + i * timedelta(days=1) for i in range(0, diff.days)]
+days = [now + i * timedelta(days=1) for i in range(0, diff.days + 1)]
 
 # Make list of paydays (this arbitrarily starts on May 11th 2018)
 paydayZero = date(2018, 4, 27)
@@ -32,11 +43,12 @@ paydays = [paydayZero + i * timedelta(days=14) for i in range(0, int(diff.days))
 
 # Find how many paydays are in days and calculate the amount of vacation time
 paydayCount = 0
-for d in range(0, diff.days):
+for d in range(0, diff.days + 1):
     for pd in range(0, int(diff.days)):
         if days[d] == paydays[pd]:
             paydayCount += 1
+
 vacationTime += paydayCount * 7.3845
 
 # Print vacation balances
-print("You will have %.1f days of vacation time by %s/%s/%s." % (vacationTime / 8, vacationMonth, vacationDay, vacationYear))
+print("You will have %.1f days (%.1f hours) of vacation time by %s/%s/%s." % (vacationTime / 8, vacationTime, vacationMonth, vacationDay, vacationYear))
